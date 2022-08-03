64830dc8554421b539f7ff910d969053f37b4870
Poul Kjeldager Sørensen <pks@delegate.dk>
2022-08-03 22:59:40 +0200

    feat: Added workflow entity also

--- src/EAVFW.Extensions.WorkflowEngine/IWorkflowRun.cs
+++ src/EAVFW.Extensions.WorkflowEngine/IWorkflowRun.cs
@@ -1,4 +1,5 @@
 ﻿using DotNetDevOps.Extensions.EAVFramework.Shared;
+using EAVFW.Extensions.Documents;
 using System;
 
 namespace EAVFW.Extensions.WorkflowEngine
@@ -8,5 +9,14 @@ namespace EAVFW.Extensions.WorkflowEngine
     {
         Byte[] State { get; set; }
         Guid Id { get; set; }
+    }
+
+    [EntityInterface(EntityKey = "Workflow")]
+    public interface IWorkflow<T> where T : IDocumentEntity
+    {
+        public Guid Id { get; set; }        
+        public string Version { get; set; }
+        public Guid? ManifestId { get; set; }
+        public T Manifest { get; set; }
     }
 }
