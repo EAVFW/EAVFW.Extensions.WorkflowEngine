2e4161d5b5a3aa162a35bbc0c543e1302f6621e6
Poul Kjeldager Sørensen <pks@delegate.dk>
2023-10-29 14:17:21 +0100

    fix: changed the defaults to include, most usecase want them and they have auth handled in so if not implementing auth handlers it would return 401 until fixed.

--- src/EAVFW.Extensions.WorkflowEngine/WorkflowEndpointOptions.cs
+++ src/EAVFW.Extensions.WorkflowEngine/WorkflowEndpointOptions.cs
@@ -3,7 +3,7 @@ namespace EAVFW.Extensions.WorkflowEngine
     public class WorkflowEndpointOptions
     {
         public bool IncludeListWorkflows { get; set; } = false;
-        public bool IncludeStartWorkflow { get; set; } = false;
-        public bool IncludeWorkflowState { get; set; } = false;
+        public bool IncludeStartWorkflow { get; set; } = true;
+        public bool IncludeWorkflowState { get; set; } = true;
     }
 }
